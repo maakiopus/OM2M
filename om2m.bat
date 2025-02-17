0ed654d4fa52485a1421b655fee53c367833b82b
Bala Likhith Kanigolla <55309319+likhithkanigolla@users.noreply.github.com>
2025-02-17 10:40:11 +0530

    Update om2m.bat

--- om2m.bat
+++ om2m.bat
@@ -1,3 +1,3 @@
 d:
 cd "D:\PATH\TO\in-cse"
-"../java8/bin/java" -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
+"PATH/TO/java8/bin/java" -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
