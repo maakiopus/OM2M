2e33e8a060a3d8539a156b29c20ef5ad708fce9c
Hemanth Nandipati <maaki@duck.com>
2025-05-15 15:31:19 +0530

    update start scripts updates the windows and unix scripts to dynamically determine directory and find java binary also renames windows bat script to semantically match with linux's

--- om2m.bat
+++
@@ -1,3 +0,0 @@
-d:
-cd "D:\PATH\TO\in-cse"
-"PATH/TO/java8/bin/java" -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+SCRIPT_DIR=$(dirname "$0")
 # Navigate to the directory where the Eclipse launcher JAR is located
-cd /PATH/TO/THE/FILES/OF/OM2M
+cd $SCRIPT_DIR	
+export JAVAPATH=$(which java) 
+echo "Java path found: $JAVAPATH"
+
+if [[ -z $JAVAPATH ]] ; then
+	echo "No java found, Exiting"
+	exit 1
+fi
 
 # Run the Java program
-"/PATH/TO/THE/JAVA/java_1.8/Contents/Home/jre/bin/java" -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
+"$JAVAPATH" -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
 
